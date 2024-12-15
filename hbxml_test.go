db26b87d76654f270bfe64492346ef8d14baea8d
Lekuru <contact@lekuru.xyz>
2024-12-15 14:43:26 +0100

    Add combo testing

--- hbxml_test.go
+++ hbxml_test.go
@@ -21,6 +21,38 @@ func TestBeatmapDecoding(t *testing.T) {
 		_, err = ParseFromFile(fmt.Sprintf("examples/%s", file.Name()))
 		if err != nil {
 			t.Error(err)
+			return
+		}
+	}
+}
+
+func TestCombo(t *testing.T) {
+	comboMap := map[string]int{
+		"LeaF - NANO DEATH!!!!! (NerdNerdTewt) [Death].hbxml":                                   87,
+		"Drop - Granat (TheHowl) [Pro].hbxml":                                                   214,
+		"RhythmHolic - GLP Dubstep Intro (ProPlayer08) [Pro].hbxml":                             163,
+		"RhythmHolic - GLP Dubstep Intro (ProPlayer08) [EZ].hbxml":                              132,
+		"NJK Record - Dirty Sexy Girl (Nicholas) [BASIC].hbxml":                                 702,
+		"NJK Record - Dirty Sexy Girl (Nicholas) [NO HOLDS].hbxml":                              548,
+		"Hige Driver join. SELEN - Dadadadadadadadadada (Nicholas) [EXPERT].hbxml":              470,
+		"Kuroma - Feelings beloved overheat (Takuya) [Expert].hbxml":                            1378,
+		"shisotex - Final Fantasy III Sailing Enterprise ~ The Invincible (Takuya) [Pro].hbxml": 1198,
+	}
+
+	for file, combo := range comboMap {
+		b, err := ParseFromFile("examples/" + file)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		caclulatedCombo := b.ComputeMaxCombo()
+		difference := combo - caclulatedCombo
+
+		if difference != 0 {
+			t.Errorf(
+				"WARNING: Expected %d, got %d (%d): %s\n",
+				combo, caclulatedCombo, difference, b.FormatName(),
+			)
 		}
 	}
 }
